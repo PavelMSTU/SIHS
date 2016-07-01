e84ce52254ee0b2ea8af3c2297a988cc7fbc6823
PavelMSTU <PavelMSTU@stego.su>
2016-07-02 00:18:55 +0300

    + make_db + sihs_hash

--- Core.py
+++ Core.py
@@ -8,9 +8,16 @@
     I am SOMBE!
 """
 
+import os
 import sys
 import datetime
+import sqlite3
+import ConfigParser
 from time import sleep
+from functools import partial
+
+# you can use another hash. See sihs_hash docstring
+import hashlib
 
 __author__ = u'PavelMSTU'
 
@@ -21,6 +28,35 @@ IMAGE_HASH_FOLDER = u'image_hash_stego'
 
 # Input folder. Входная папка
 IMAGE_STORE = u'image_store'
+
+DB_PATH = os.path.join(IMAGE_STORE, u'IMAGEDB.sqlite.db')
+
+
+def sihs_hash(file_path, count_bytes=1):
+    """
+    Some hash func.
+    You can CHANGE it, if you want.
+
+    For example, you can use IMAGE-HASH,
+        not cryptography hash.
+    If you do it, you can build ROBUST
+    steganography system.
+
+    :param file_path:
+    path of some some bytes
+    :param count_bytes: count of bytes
+    :return:
+    return hash
+    """
+
+    d = hashlib.md5()
+    with open(file_path, 'rb') as file_b:
+        for buf in iter(partial(file_b.read, 128), b''):
+            d.update(buf)
+
+    a = d.hexdigest()
+    val = a[0:count_bytes*2]
+    return int(val, 16)
 
 
 def generate_message_chain(
@@ -70,10 +106,77 @@ def __test1():
             raise RuntimeError(u'Core is not work!')
     return True
 
+
+def make_db(
+    image_store_folder=IMAGE_STORE,
+    db_path=DB_PATH,
+):
+    """
+    Function for build DB
+
+    ATTANTION:
+        when you added some image files to IMAGE_STORE
+        you must call this function
+
+    :param image_store_folder: folder, where images is store.
+    :param db_path: path of DB
+    :return:
+    return count of images in DB
+    """
+
+    config = ConfigParser.RawConfigParser()
+    config.read(os.path.join(IMAGE_STORE, u'README'))
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        u"""
+        CREATE TABLE IMAGES
+                -- Table of image path and its HASH value. Using for Hash-steganography
+            (
+                id INTEGER PRIMARY KEY,
+                img_path TEXT, -- Path of image regarding IMAGE_STORE={0} folder
+                img_hash_1b INTEGER -- ONE byte of hash
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        """.format(image_store_folder)
+    )
+    conn.commit()
+
+    count_insert = 0
+    for file_ in os.listdir(IMAGE_STORE):
+        file_path = os.path.join(IMAGE_STORE, file_)
+
+        if file_path.split('.')[-1] not in config.get('main', 'format'):
+            # this file is not correct format
+            continue
+
+        # read all bytes of some file
+        hash_ = sihs_hash(file_path)
+        cursor.execute(
+            u"""
+            INSERT INTO IMAGES
+                (img_path, img_hash_1b)
+                VALUES
+
+                ('{0}', {1})
+            """.format(file_path, hash_)
+        )
+        conn.commit()
+        # TODO check the errors of cursor.execute!
+        count_insert += 1
+        print u"{0}: Add '{1}' image to DB".format(count_insert, file_path)
+
+    conn.close()
+    return count_insert
+
+
 if __name__ == u"__main__":
     print u"TEST Core " + unicode(datetime.datetime.now())
     sleep(3)
 
-    __test1()
+    make_db()
+    # __test1()
 
 
